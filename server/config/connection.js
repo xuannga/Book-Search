a02552ba2a2d043be2e2c68cf4c374392d5c4b08
xuannga <nguyenx206@gmail.com>
2021-10-13 17:36:31 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookSearch', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+}, );
 
 module.exports = mongoose.connection;
