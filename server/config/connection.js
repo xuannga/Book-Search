d4c5ae1cd9c26b4180ead2ca71c2dda9cbff3536
xuannga <nguyenx206@gmail.com>
2021-10-17 20:34:11 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookSearch', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BOOKSEARCH', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
